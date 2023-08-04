13789911f8e5f5f5918b001e5ea7250d75fc8430
Lee Macdonald <lee.macdonald@vector.co.nz>
2023-08-05 11:48:09 +1200

    use an f string to do the rounding

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 x = float(input("What's x?: "))
 y = float(input("What's y?: "))
-z = round(x + y, 3)
-print(z)
+z = x / y
+#the f string tells python to round it to two decimal places. 
+print(f"{z:.2f})
