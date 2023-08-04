34d26af421a5f26f2477c929d0ec5bb4ae6519d9
Lee Macdonald <lee.macdonald@vector.co.nz>
2023-08-05 11:54:11 +1200

    formatting numbers with , for readability

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 x = float(input("What's x?: "))
 y = float(input("What's y?: "))
-z = x / y
+z = x * y
 #the f string tells python to round it to two decimal places. 
-print(f"{z:.2f})
+print(f"{z:,}")
