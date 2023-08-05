720aa56c4249afb581bcd8a0a9531bf2888b41d9
Lee Macdonald <lee.macdonald@vector.co.nz>
2023-08-05 13:08:36 +1200

    playing with function default values

--- diyfunctions.py
+++ diyfunctions.py
@@ -1,9 +1,10 @@
-#Define your function
-def hello():
+#Define your function. after "to", optional default argument of "world"
+def hello(to = "world"):
     #Define what your function does
-    print("hello")
+    print("hello,", to)
+#Hello function with no argument, will use the default in line 1
+hello()
 #Ask user their name. Remove whitespace from string. Capitalise users name
 name = input("What's your name?: ")
 #use the function
-hello()
-print(name)
+hello(name)
