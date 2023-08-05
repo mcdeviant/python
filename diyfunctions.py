de7c3f37f5d0a200771b54aa26ba83c2f72e3490
Lee Macdonald <lee.macdonald@vector.co.nz>
2023-08-05 12:56:46 +1200

    add comments

--- diyfunctions.py
+++ diyfunctions.py
@@ -1,6 +1,9 @@
+#Define your function
 def hello():
+    #Define what your function does
     print("hello")
 #Ask user their name. Remove whitespace from string. Capitalise users name
 name = input("What's your name?: ")
+#use the function
 hello()
 print(name)
