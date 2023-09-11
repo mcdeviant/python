1d840f07655533b16aed08da9d53a9428c3e6cc0
Lee Macdonald <lee.macdonald@vector.co.nz>
2023-09-11 19:34:29 +1200

    Task 3 - convert emoticons to emoji

--- faces/face.py
+++ faces/face.py
@@ -1,17 +1,19 @@
 #!/bin/python
 
+#Creates a function to convert emoticons to emojis
+def convert(input_string):
+    #Stores the replacement for :) into "smile" variable
+    smile = (input_string.replace(":)","\U0001F642"))
+    #Adds the :( conversion and stores it all in the "frown" variable
+    frown = (smile.replace(":(","\U0001F641"))
+    #When the function is called, this tells it to return the "frown" variable
+    return frown
+    
 def main():
-
-    #convert input to string, store in variable
-    STRING = str(input())
-    #call convert on the above variable
-    RESULT = convert(STRING)
-
-#Converts emoticons to emojis
-def convert(STRING):
-    SMILE = STRING.replace(":)","\\U0001F642")
-    FROWN = STRING.replace(":(","\\1F641")
-    return STRING
-convert()
+    #convert input to string, store in "input_string" variable
+    input_string = str(input())
+    #call convert on the above variable, store in "result" variable
+    result = convert(input_string)
+    print(result)
 
 main()
