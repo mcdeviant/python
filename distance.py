a731e4c33bd21945a81319c093f2d5e5b000cf9c
Lee Macdonald <lee.macdonald@vector.co.nz>
2023-10-10 21:58:16 +1300

    More function and fstring practice

---
+++ distance.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+def main():
+    miles = miles_input(input("Enter number of miles to convert: "))
+    kms = kms_out(miles * 1.6)
+    print(f"{miles:.2f} miles = {kms:.2f} kilometers")
+
+def miles_input(m):
+    return int(float(m))
+
+def kms_out(k):
+    return (float(k))
+
+main()
