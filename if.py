5a251fb0a7389c2382e0dab38c6561df443b59c6
Lee Macdonald <lee.macdonald@vector.co.nz>
2023-10-16 19:56:00 +1300

    minor improvements to the code structure.

--- if.py
+++ if.py
@@ -1,9 +1,12 @@
 x = int(input("What is x? "))
 y = int(input("What is y? "))
 
-if ( x < y ) and ( x > 0 ):
-    print(f"x is between 0 and {y} - y.")
-elif ( x > y ) or ( x < 0 ):
-    print(f"x is greater than {y} - y, or less than 0.")
+
+if x > y :
+    print(f"x ({x}) is greater than y ({y}).")
+elif x == y :
+    print(f"x ({x}) is equal to y ({y})")
+elif x < y and x > 0 :
+    print(f"x ({x}) is between 0 and y ({y}).")
 else:
-    print( "x is equal to y or 0.")
+    print(f"x ({x}) is less than or equal to 0.")
