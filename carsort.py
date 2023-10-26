057f4a70af480c36d28e88c11f859b0995a7d471
Lee Macdonald <lee.macdonald@vector.co.nz>
2023-10-26 21:23:06 +1300

    not working yet, made a few changes

--- carsort.py
+++ carsort.py
@@ -1,15 +1,17 @@
-#!/bin/bash
+#!/usr/bin/python
 
 def main():
     car_info = { 
-    colour : str(input("What colour is your car?: "))
-    }
+      str(input("What colour is your car?: "))
+      } #store user input in variable car_info
 
-    if is_dark(x):
+    if is_dark(car_info): #pass the variable to is_dark function
         print("Insurance will be higher.")
     else:
         print("Insurance will be lower.")
 
-def is_dark(d):
-    dark_colours = ["black", "dark"]
-    return any(dark_colour in d.lower() for dark_colour in dark_colours)
+def is_dark(colour): # set is_dark funnction with (d) parameter
+    dark_colours = ["black", "dark"] #set criteria for the function to be True
+    return any(dark_colour in colour.lower() for dark_colour in dark_colours) #conver user input to lower case
+
+main()
