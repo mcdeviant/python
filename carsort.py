99ad9fc7813ef76e9f2cd6974c3e7729ba79ab30
Lee Macdonald <lee.macdonald@vector.co.nz>
2023-10-27 19:59:03 +1300

    Working with colour sorting now

--- carsort.py
+++ carsort.py
@@ -2,16 +2,16 @@
 
 def main():
     car_info = { 
-      str(input("What colour is your car?: "))
+      "colour": str(input("What colour is your car?: ")),
       } #store user input in variable car_info
 
-    if is_dark(car_info): #pass the variable to is_dark function
-        print("Insurance will be higher.")
+    if is_dark(car_info["colour"]): #pass the variable to is_dark function
+        print(f"Insurance will be higher for your {car_info['colour']} car.")
     else:
-        print("Insurance will be lower.")
+        print(f"Insurance will be lower for your {car_info['colour']} car.")
 
-def is_dark(colour): # set is_dark funnction with (d) parameter
+def is_dark(colour): # set is_dark funnction with (colour) parameter
     dark_colours = ["black", "dark"] #set criteria for the function to be True
-    return any(dark_colour in colour.lower() for dark_colour in dark_colours) #conver user input to lower case
+    return any(dark_colour in colour.lower() for dark_colour in dark_colours) #convert user input to lower case
 
 main()
