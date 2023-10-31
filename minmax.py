6e42c8097fae326f733a48db69a474348404284b
Lee Macdonald <lee@mcdeviant.com>
2023-10-31 20:55:13 +1300

    test

---
+++ minmax.py
@@ -0,0 +1,5 @@
+def find_max(lst):
+    return max(lst)
+
+numbers = [4, 2, 9, 6, 5, 1, 8, 3, 7]
+print(find_max(numbers))  
--- parity.py
+++ parity.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 x = int(input("what is x? "))
 
