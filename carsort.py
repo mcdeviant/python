4a5baf2a9f385e0487c91a9f3f2c8a4b84e050ef
Lee Macdonald <lee@mcdeviant.com>
2023-11-08 20:21:11 +1300

    add filter for higher end cars to be more expensive

--- carsort.py
+++ carsort.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+#I wasn't happy with my function understanding, and wanted to revise it, so made this to practice. 
+#If you enter a colour with "dark" or "black", it will report higher interest. 
 def main(): 
     
     car_info = {
@@ -8,13 +9,17 @@ def main():
         }
     car_info["model"] = str(input(f"What model is your {car_info['make']}?: ")) #References the 'make', so I had to complete/close the previous dictionary assignments.
 
-    if is_dark(car_info["colour"]): #pass the variable to is_dark function
+    if is_dark(car_info["colour"]) or is_rich(car_info["make"]): #pass the variable to is_dark function
         print(f"Insurance will be higher for your {car_info['colour']}, {car_info['make']}, {car_info['model']}.")
     else:
         print(f"Insurance will be lower for your {car_info['colour']}, {car_info['make']}, {car_info['model']}.")
 
-def is_dark(colour): # set is_dark funnction with (colour) parameter
-    dark_colours = ["black", "dark"] #set criteria for the function to be True
-    return any(dark_colour in colour.lower() for dark_colour in dark_colours) #convert user input to lower case
+def is_dark(colour): # set is_dark function with (colour) parameter
+    dark_colours = ["black", "dark"] #set criteria for the bool function to be True
+    return any(dark_colour in colour.lower() for dark_colour in dark_colours) #convert user input to lower case to ensure match with criteria above
+
+def is_rich(make): # set is_rich function with (make) parameter
+    rich_models = ["mercedes", "bmw", "lexus", "aston martin", "ferrari", "porsche", "lamborghini", "tesla"] #set car models for bool function to be True
+    return any(rich_model in make.lower() for rich_model in rich_models) #convert the user input to lower case to ensure match
 
 main()
