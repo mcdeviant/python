080b7d68a33e3aaee9c5d89542f1ca37dcd4c48c
Lee Macdonald <lee@mcdeviant.com>
2023-12-02 21:22:47 +1300

    testing personal laptop git setup

--- carsort.py
+++ carsort.py
@@ -8,13 +8,13 @@ def main():
         }
     car_info["model"] = str(input(f"What model is your {car_info['make']}?: ")) #References the 'make', so I had to complete/close the previous dictionary assignments.
 
-    if is_dark(car_info["colour"]): #pass the variable to is_dark function
+    if is_dark(car_info["colour"]): #pass the variable to is_dark function.
         print(f"Insurance will be higher for your {car_info['colour']}, {car_info['make']}, {car_info['model']}.")
     else:
         print(f"Insurance will be lower for your {car_info['colour']}, {car_info['make']}, {car_info['model']}.")
 
-def is_dark(colour): # set is_dark funnction with (colour) parameter
-    dark_colours = ["black", "dark"] #set criteria for the function to be True
-    return any(dark_colour in colour.lower() for dark_colour in dark_colours) #convert user input to lower case
+def is_dark(colour): # set is_dark funnction with (colour) parameter.
+    dark_colours = ["black", "dark"] #set criteria for the function to be True.
+    return any(dark_colour in colour.lower() for dark_colour in dark_colours) #convert user input to lower case.
 
 main()
