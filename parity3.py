a1a1e9f71f11611f7d69db1f9689d183cba83a04
Lee Macdonald <lee@mcdeviant.com>
2023-12-05 10:39:31 +1300

    update shebang

---
+++ parity3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    return n % 2 == 0
+
+
+main()
